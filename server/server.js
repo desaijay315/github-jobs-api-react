6bf635b74308cd3d94880e7abde127d2b7932c8e
desaijay315 <desaijay315@gmail.com>
2020-07-26 16:59:23 +0530

    make it better

--- server/server.js
+++ server/server.js
@@ -3,13 +3,12 @@ const app = express();
 
 const path = require('path');
 const port = process.env.PORT || 5000;  
-const publicPath = path.join(__dirname, '..', 'build');
 
-app.use(express.static(publicPath));
+// add middlewares
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
